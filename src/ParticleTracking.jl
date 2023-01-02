33ed4c17e35e6c9499fe60317a7bc6f5a83e44ce
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-01-02 15:07:08 +0100

    add some shorthand notations for internal use

--- src/ParticleTracking.jl
+++ src/ParticleTracking.jl
@@ -6,4 +6,11 @@ using OffsetArrays
 using Hungarian
 using Images
 
+# shorthand notations
+CI = CartesianIndex
+CC{N} = Vector{CI{N}} # connected component
+Stack{T,N} = Vector{Array{T,N}}
+Stack2{T} = Stack{T,2}
+Stack3{T} = Stack{T,3}
+
 end
