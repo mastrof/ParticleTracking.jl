60ca079b731e64fdbf97a859cc801b87cd207f18
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-12-16 14:18:37 +0100

    comments

--- src/makie_recipes.jl
+++ src/makie_recipes.jl
@@ -1,8 +1,11 @@
-function MakieCore.convert_arguments(P::Type{<:Lines}, x::Trajectory, t::Integer, t0::Integer)
+# draw a trajectory
+MakieCore.convert_arguments(P::Type{<:Lines}, x::Trajectory, t::Integer, t0::Integer) =
     (Point2f.(location.(x[max(1,t-t0):t])),)
-end
+
+# draw a sequence of blobs as a trajectory
 MakieCore.convert_arguments(P::Type{<:Lines}, x::AbstractVector{<:AbstractBlob}) =
     (Point2f.(location.(x)),)
 
+# draw blobs as scatter points
 MakieCore.convert_arguments(P::Type{<:Scatter}, x::AbstractVector{<:AbstractBlob}) =
     (Point2f.(location.(x)),)
