de73820ff57bbbc4226d59751e7f3b24b350c055
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-12-16 17:29:14 +0100

    block rare but bugged branch in location refinement

--- src/location_refinement.jl
+++ src/location_refinement.jl
@@ -24,13 +24,18 @@ function offsets(blob::Blob{T,S,2}) where {T,S}
     end
     εx /= m0
     εy /= m0
+    # TODO: needs fixing but it's a rare case so can be probably omitted for now.
+    # When new_location is set it does not coincide with the center of intensity map;
+    # on the following iteration indexing is messed up and goes out of bounds
+    #==
     if abs(εx) > 0.5 || abs(εy) > 0.5
-        new_location = CartesianIndex(x + 1*sign(εx), y + 1*sign(εy))
+        new_location = CartesianIndex(x + 1*Int(sign(εx)), y + 1*Int(sign(εy)))
         new_blob = Blob(new_location, scale(blob), amplitude(blob),
             zeroth_moment(blob), second_moment(blob), intensity_map(blob)
         )
         offsets(new_blob)
     end
+    ==#
     return (εx, εy)
 end
 offsets(blob::BlobRefined{T,S,N}) where {T,S,N} = ntuple(_ -> 0.0, N)
