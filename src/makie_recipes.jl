8d4d00c551bc060044bd828996c22a3192cce20a
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-12-18 00:39:44 +0100

    add recipe to plot entire trajectory

--- src/makie_recipes.jl
+++ src/makie_recipes.jl
@@ -1,6 +1,9 @@
-# draw a trajectory
+# draw a trajectory up to time t with a tail of length t0
 MakieCore.convert_arguments(P::Type{<:Lines}, x::Trajectory, t::Integer, t0::Integer) =
     (Point2f.(location.(x[max(1,t-t0):t])),)
+# draw entire trajectory
+MakieCore.convert_arguments(P::Type{<:Lines}, x::Trajectory) =
+    (Point2f.(location(x)),)
 
 # draw a sequence of blobs as a trajectory
 MakieCore.convert_arguments(P::Type{<:Lines}, x::AbstractVector{<:AbstractBlob}) =
