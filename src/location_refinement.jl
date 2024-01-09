754ebb23318d010c7e33658088db6fad1f2391e4
Riccardo Foffi <riccardo.foffi@protonmail.com>
2024-01-09 21:05:04 +0100

    fix reiteration in subpixel localization

--- src/location_refinement.jl
+++ src/location_refinement.jl
@@ -16,22 +16,18 @@ function offsets(blob::BlobRaw{T,S,N}) where {T,S,N}
     x = location(blob).I
     ε = zeros(N)
     for c in CartesianIndices(I)
-        delta = x .- c.I
+        delta = c.I .- x
         ε .+= delta .* I[CartesianIndex(x .+ delta)]
     end
     ε ./= m0
-    # BUG: was bugged with previous 2D implementation; should work now but needs check.
-    # When new_location is set it does not coincide with the center of intensity map;
-    # on the following iteration indexing is messed up and goes out of bounds
-    #==
     if any(abs(d) > 0.5 for d in ε)
-        new_location = CartesianIndex(@. x + Int(sign(ε)))
+        # reassign the dimensions for which ε>1/2
+        new_location = CartesianIndex(Tuple(@. x + round(Int, ε)))
         new_blob = BlobRaw(new_location, scale(blob), amplitude(blob),
             zeroth_moment(blob), second_moment(blob), intensity_map(blob)
         )
         offsets(new_blob)
     end
-    ==#
     return Tuple(ε)
 end
 offsets(blob::Blob{T,S,N}) where {T,S,N} = ntuple(_ -> 0.0, N)
