ab90b885a1e49941ddc7df6f416042243670a886
Riccardo Foffi <riccardo.foffi@protonmail.com>
2025-05-07 12:01:16 +0200

    simplify default show method

--- src/blobs.jl
+++ src/blobs.jl
@@ -7,13 +7,7 @@ Abstract interface for blob types
 abstract type AbstractBlob{T,S,N} end
 function Base.show(io::IO, blob::AbstractBlob)
     l = location(blob)
-    σ = scale(blob)
-    a = amplitude(blob)
-    m0 = zeroth_moment(blob)
-    m2 = second_moment(blob)
-    print(io, 
-        "Blob(location=$l, σ=$σ, amplitude=$a, m₀=$m0, m₂=$m2)"
-    )
+    print(io, "Blob(location=$l)")
 end
 
 
