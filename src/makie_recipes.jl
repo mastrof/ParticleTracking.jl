d59d395316549a92e13c6ebfdd969ced37639366
Riccardo Foffi <riccardo.foffi@protonmail.com>
2025-05-07 23:26:28 +0200

    formatting

--- src/makie_recipes.jl
+++ src/makie_recipes.jl
@@ -1,14 +1,28 @@
+# draw blobs as scatter points
+function MakieCore.convert_arguments(
+    P::Type{<:Scatter},
+    x::AbstractVector{<:AbstractBlob}
+)
+    (Point2f.(location.(x)),)
+end
+
+# draw a sequence of blobs as a trajectory
+function MakieCore.convert_arguments(
+    P::Type{<:Lines},
+    x::AbstractVector{<:AbstractBlob}
+)
+    (Point2f.(location.(x)),)
+end
+
 # draw a trajectory up to time t with a tail of length t0
-MakieCore.convert_arguments(P::Type{<:Lines}, x::Trajectory, t::Integer, t0::Integer) =
+function MakieCore.convert_arguments(
+    P::Type{<:Lines},
+    x::Trajectory,
+    t::Integer,
+    t0::Integer
+)
     (Point2f.(location.(x[max(1,t-t0):t])),)
+end
 # draw entire trajectory
 MakieCore.convert_arguments(P::Type{<:Lines}, x::Trajectory) =
     (Point2f.(location(x)),)
-
-# draw a sequence of blobs as a trajectory
-MakieCore.convert_arguments(P::Type{<:Lines}, x::AbstractVector{<:AbstractBlob}) =
-    (Point2f.(location.(x)),)
-
-# draw blobs as scatter points
-MakieCore.convert_arguments(P::Type{<:Scatter}, x::AbstractVector{<:AbstractBlob}) =
-    (Point2f.(location.(x)),)
