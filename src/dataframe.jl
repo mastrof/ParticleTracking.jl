f05b2281320ba1529abca8ee7704376761b11985
Riccardo Foffi <rfoffi@ethz.ch>
2025-10-20 20:55:21 +0200

    feature: implement dataframe conversion for blobs, rename column m0 -> mass

--- src/dataframe.jl
+++ src/dataframe.jl
@@ -5,23 +5,9 @@ export to_df
 Convert a set of trajectories into a dataframe.
 """
 function to_df(tracks::AbstractVector{<:Trajectory})
-    df = DataFrame(;
-        frame=Int[],
-        particle=Int[],
-        x=Float64[],
-        y=Float64[],
-        m0=Float64[],
-        m2=Float64[],
-        size=Float64[],
-    )
-    foreach((track, id) -> append!(df, to_df(track, id)), tracks, 1:length(tracks))
-    df
+    vcat(map(to_df, tracks, eachindex(tracks))...)
 end
-"""
-    to_df(track, id)
-Convert a single track to a dataframe and assign it the given id.
-"""
-function to_df(track::Trajectory, id::Integer)
+function to_df(track::Trajectory{S,<:AbstractBlob{T,2}}, id::Integer) where {S,T}
     frame = timestamps(track)
     particle = fill(id, length(frame))
     pos = location(track)
@@ -30,5 +16,46 @@ function to_df(track::Trajectory, id::Integer)
     m0 = zeroth_moment(track)
     m2 = second_moment(track)
     s = first.(scale(track))
-    DataFrame(; frame, particle, x, y, m0, m2, size=s)
+    DataFrame(; frame, particle, x, y, mass=m0, m2, size=s)
+end
+function to_df(track::Trajectory{S,<:AbstractBlob{T,3}}, id::Integer) where {S,T}
+    frame = timestamps(track)
+    particle = fill(id, length(frame))
+    pos = location(track)
+    x = first.(pos)
+    y = getindex.(pos, 2)
+    z = last.(pos)
+    m0 = zeroth_moment(track)
+    m2 = second_moment(track)
+    s = first.(scale(track))
+    DataFrame(; frame, particle, x, y, z, mass=m0, m2, size=s)
+end
+
+"""
+    to_df(blobs::AbstractVector{<:AbstractVector{<:AbstractBlob}})
+Convert a vector of blob detections into a dataframe.
+"""
+function to_df(blobs::AbstractVector{<:AbstractVector{<:AbstractBlob}})
+    vcat(map(to_df, blobs, eachindex(blobs))...)
+end
+function to_df(blobs::AbstractVector{<:AbstractBlob{T,2}}, t::Integer) where T
+    frame = fill(t, length(blobs))
+    pos = location.(blobs)
+    x = first.(pos)
+    y = last.(pos)
+    m0 = zeroth_moment.(blobs)
+    m2 = second_moment.(blobs)
+    s = first.(scale.(blobs))
+    DataFrame(; frame, x, y, mass=m0, m2, size=s)
+end
+function to_df(blobs::AbstractVector{<:AbstractBlob{T,3}}, t::Integer) where T
+    frame = fill(t, length(blobs))
+    pos = location.(blobs)
+    x = first.(pos)
+    y = getindex.(pos, 2)
+    z = last.(pos)
+    m0 = zeroth_moment.(blobs)
+    m2 = second_moment.(blobs)
+    s = first.(scale.(blobs))
+    DataFrame(; frame, x, y, z, mass=m0, m2, size=s)
 end
